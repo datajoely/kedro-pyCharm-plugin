464cd6cdd34694795efa48aa0c5c17128e5df413
datajoely <joel.schwarzmann@quantumblack.com>
2020-06-01 22:35:17 +0100

    Streamlined main autocomplete class

--- src/main/kotlin/com/quantumblack/kedro/KedroNodeClassAutoComplete.kt
+++ src/main/kotlin/com/quantumblack/kedro/KedroNodeClassAutoComplete.kt
@@ -1,32 +1,39 @@
 package com.quantumblack.kedro
 
-
-import com.intellij.codeInsight.completion.*
-import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.openapi.util.IconLoader
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
 import com.jetbrains.python.PythonLanguage
-import com.jetbrains.python.psi.*
+import com.jetbrains.python.psi.PyCallExpression
+import com.jetbrains.python.psi.PyExpression
+import com.jetbrains.python.psi.PyListLiteralExpression
+import com.jetbrains.python.psi.PyReferenceExpression
+import com.jetbrains.python.psi.PyStringLiteralExpression
+import com.jetbrains.python.psi.impl.PyReferenceExpressionImpl
 import com.jetbrains.python.psi.impl.PyStringLiteralExpressionImpl
-
+import javax.swing.Icon
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.annotations.Nullable
-import javax.swing.Icon
 
 class KedroNodeClassAutoComplete : CompletionContributor() {
     init {
-        extend(CompletionType.BASIC,
-
+        val icon: Icon = IconLoader.getIcon("/icons/kedroIcon_dark.svg")
+        extend(
+            CompletionType.BASIC,
             PlatformPatterns.psiElement().withLanguage(PythonLanguage.INSTANCE),
-            object : CompletionProvider<CompletionParameters?>() {
+            object : CompletionProvider<CompletionParameters>() {
+
                 override fun addCompletions(
                     parameters: @NotNull CompletionParameters,
                     context: @NotNull ProcessingContext,
                     resultSet: @NotNull CompletionResultSet
                 ) {
-                    val kedroIcon: Icon = getIconInCorrectColourScheme(parameters)
 
                     val call: @Nullable PyCallExpression? = PsiTreeUtil.getParentOfType(
                         parameters.originalPosition,
@@ -37,63 +44,49 @@ class KedroNodeClassAutoComplete : CompletionContributor() {
                         val calleeExpression: @Nullable PyExpression? = call.callee
                         if (calleeExpression is PyReferenceExpression) {
 
-                            val isWithinNodeConstructor: Boolean = calleeExpression.reference.canonicalText.contains(
-                                other = "Node",
-                                ignoreCase = true
-                            )
                             val isKedroNodeInputOutputExpression: Boolean = detectKedroNodeInputOutputExpression(
-                                parameters,
-                                listOf(
-                                    PyReferenceExpression::class.java, // Before writing
-                                    PyStringLiteralExpression::class.java, // Writing a string
-                                    PyStringLiteralExpressionImpl::class.java, // Writing a string
-                                    PyListLiteralExpression::class.java // Writing within a list
-                                )
+                                parameters
                             )
 
-                            if (isWithinNodeConstructor && isKedroNodeInputOutputExpression) {
-                                for (i in 1 until 10) {
-                                    val datasetName = "DataSet$i"
-                                    val datasetType = "Type$i"
-                                    resultSet.addElement(
-                                        LookupElementBuilder.create("\"$datasetName\"")
-                                            .withPresentableText(datasetName)
-                                            .bold()
-                                            .withCaseSensitivity(false)
-                                            .withIcon(kedroIcon)
-                                            .withTypeText(datasetType)
-                                            .withLookupString(datasetName)
-                                    )
-                                }
-
+                            if (isKedroNodeCall(calleeExpression) && isKedroNodeInputOutputExpression) {
+                                val catalogManager = KedroDataCatalogManager(call.project, icon)
+                                resultSet.addAllElements(catalogManager.getKedroDataSetSuggestions())
                             }
-
                         }
                     }
+                }
 
-
+                private fun isKedroNodeCall(calleeExpression: PyReferenceExpression): Boolean {
+                    return calleeExpression.reference.canonicalText.contains(
+                        other = "Node",
+                        ignoreCase = true
+                    )
                 }
 
                 private fun detectKedroNodeInputOutputExpression(
-                    parameters: @NotNull CompletionParameters,
-                    expressions: List<Class<out PyExpression>>
+                    parameters: @NotNull CompletionParameters
                 ): Boolean {
+
+                    val expressions: List<Class<out PyExpression>> = listOf(
+                        PyExpression::class.java,
+                        PyReferenceExpressionImpl::class.java, // No kwargs, just args
+                        PyReferenceExpression::class.java, // Before writing
+                        PyStringLiteralExpression::class.java, // Writing a string
+                        PyStringLiteralExpressionImpl::class.java, // Writing a string
+                        PyListLiteralExpression::class.java // Writing within a list
+                    )
+
                     return expressions.any { expr: Class<out PyExpression> ->
+                        // Within a Class or Function expression
                         PsiTreeUtil.getParentOfType(parameters.position, expr)
-                            ?.parent?.reference?.canonicalText?.contains(Regex(pattern = "inputs|outputs")) ?: false
+                            ?.parent // If possible, get argument name
+                            ?.reference // Get the the parameter reference
+                            ?.canonicalText // Get the  text equivalent
+                            ?.contains(Regex(pattern = "inputs|outputs")) ?: false
+                        // todo argument position 1 or 2
                     }
                 }
-
-
-                private fun getIconInCorrectColourScheme(parameters: @NotNull CompletionParameters): @NotNull Icon {
-                    return if (parameters.editor.colorsScheme.displayName.contains(other = "dark", ignoreCase = true)) {
-                        IconLoader.getIcon("/icons/kedroIcon.svg") // Use light icon if editor is light
-                    } else {
-                        IconLoader.getIcon("/icons/kedroIcon_dark.svg") // Use dark icon if editor is dark
-                    }
-                }
-
-
-            })
+            }
+        )
     }
 }
