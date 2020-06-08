a19c883f0810fc90b0614c52651aa19745250248
datajoely <joel.schwarzmann@quantumblack.com>
2020-06-08 19:32:09 +0100

    move project helper to its own class

---
+++ src/main/kotlin/com/quantumblack/kedro/PyCharmUtilities.kt
@@ -0,0 +1,33 @@
+package com.quantumblack.kedro
+
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.yaml.YAMLFileType
+import java.util.concurrent.TimeoutException
+
+object PyCharmProjectManager { // Singleton
+
+    internal var project: Project? = getProject()
+
+    private fun getProject(): Project? {
+
+        return try {
+            val dataContext: DataContext? = DataManager.getInstance().dataContextFromFocusAsync.blockingGet(2000)
+            dataContext?.getData(PlatformDataKeys.PROJECT)
+        } catch (e: TimeoutException) {
+            null
+        }
+    }
+
+    fun getScope(project: Project): GlobalSearchScope {
+        return GlobalSearchScope.getScopeRestrictedByFileTypes(
+            GlobalSearchScope.allScope(project),
+            YAMLFileType.YML
+        )
+    }
+
+
+}
