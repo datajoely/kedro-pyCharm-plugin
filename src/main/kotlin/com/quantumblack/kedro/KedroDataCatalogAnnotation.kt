f8432a65df0487beb69ba92a7f5c556a1c707647
datajoely <joel.schwarzmann@quantumblack.com>
2020-06-22 23:36:48 +0100

    Fixed annotations

--- src/main/kotlin/com/quantumblack/kedro/KedroDataCatalogAnnotation.kt
+++ src/main/kotlin/com/quantumblack/kedro/KedroDataCatalogAnnotation.kt
@@ -4,6 +4,7 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.impl.TextRangeInterval
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.elementType
 import com.jetbrains.python.PyElementTypes
@@ -23,11 +24,11 @@ class KedroDataCatalogAnnotation : Annotator {
 
                 if (isDataSetCheck){
                     val dataSetObject: KedroDataSet = KedroDataCatalogManager.get(element.text, element.project)
-                    val layer : String = ""//if (dataSetObject.layer != null) " (${dataSetObject.layer})" else ""
+                    val layer : String = if (dataSetObject.layer != null) " (${dataSetObject.layer})" else ""
                     holder.newAnnotation(HighlightSeverity.INFORMATION, "Catalog reference")
-                        //.tooltip("${dataSetObject.type}{$layer}")
+                        .tooltip("${dataSetObject.type}${layer}")
                         .textAttributes(DefaultLanguageHighlighterColors.METADATA)
-                        .range(element.textRange)
+                        .range(TextRangeInterval(element.textRange.startOffset+1, element.textRange.endOffset-1))
                         .create()
                 }
 
